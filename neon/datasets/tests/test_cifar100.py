bd8ad56c6528a6f072dd5d5ccce46a53f562e7b0
seba-1511 <seba-1511@hotmail.com>
2015-07-04 00:17:37 -0700

    Implemented tests (default and params) for split_set.

--- neon/datasets/tests/test_cifar100.py
+++ neon/datasets/tests/test_cifar100.py
@@ -4,6 +4,7 @@
 import os
 import shutil
 from nose.plugins.attrib import attr
+from math import floor
 
 from neon.datasets.cifar100 import CIFAR100
 from neon.backends.cpu import CPU
@@ -41,3 +42,34 @@ class TestCIFAR100(object):
         data.load()
         assert len(data.inputs['train']) == 50000
         assert len(data.targets['train'][0]) == 20
+
+    @attr('slow')
+    def test_split_set_default(self):
+        split = 0.2
+        data = CIFAR100(coarse=False, repo_path=self.tmp_repo)
+        data.backend = CPU(rng_seed=0)
+        data.backend.actual_batch_size = 128
+        par = NoPar()
+        par.associate(data.backend)
+        data.load()
+        expected_valid_split = floor(len(data.input['train']) * split)
+        expected_train_split = len(data.inputs['train']) - expected_valid_split
+        data.split_set(split)
+        assert len(data.inputs['train']) == expected_train_split
+        assert len(data.inputs['validation']) == expected_valid_split
+
+    @attr('slow')
+    def test_split_set_params(self):
+        split = 0.2
+        to_set = 'to_set'
+        data = CIFAR100(coarse=False, repo_path=self.tmp_repo)
+        data.backend = CPU(rng_seed=0)
+        data.backend.actual_batch_size = 128
+        par = NoPar()
+        par.associate(data.backend)
+        data.load()
+        expected_valid_split = floor(len(data.input['train']) * split)
+        expected_train_split = len(data.inputs['train']) - expected_valid_split
+        data.split_set(split, to_set=to_set, from_set='train')
+        assert len(data.inputs['train']) == expected_train_split
+        assert len(data.inputs[to_set]) == expected_valid_split
