53be46b12caab355f16ab9f9cc193595b093a6f6
seba-1511 <seba-1511@hotmail.com>
2015-07-14 16:33:19 -0700

    Added test for rounding in ToyImages.

--- neon/datasets/tests/test_val_split.py
+++ neon/datasets/tests/test_val_split.py
@@ -74,7 +74,23 @@ class TestValidationToyImages(object):
         assert expected_nb_valid == len(data.targets['validation'])
 
     def test_round_split(self):
-        pass
+        split = 10
+        batch_size = 30
+        data = ToyImages(validation_pct=split)
+        data.backend = CPU(rng_seed=0)
+        data.backend.actual_batch_size = batch_size
+        par = NoPar()
+        par.associate(data.backend)
+        data.load()
+        ntrain = sum(a.shape[1] for a in data.inputs['train'])
+        split /= 100.0
+        nb_batches = ntrain // batch_size
+        expected_nb_train = ceil((1.0 - split) * nb_batches)
+        expected_nb_valid = nb_batches - expected_nb_train
+        assert expected_nb_train == len(data.inputs['train'])
+        assert expected_nb_train == len(data.targets['train'])
+        assert expected_nb_valid == len(data.inputs['validation'])
+        assert expected_nb_valid == len(data.targets['validation'])
 
 if __name__ == '__main__':
     test = TestValidationToyImages()
