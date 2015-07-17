0dc14e3fd0fba055f22b7ef1faba5f9460c40eeb
seba-1511 <seba-1511@hotmail.com>
2015-07-17 00:24:47 -0700

    Re-added non useless line.

--- neon/models/mlp.py
+++ neon/models/mlp.py
@@ -134,6 +134,7 @@ class MLP(Model):
         Prints the metric score of this model on the specified dataset.
         """
         if dataset.has_set(setname):
+            self.set_train_mode(False)
             metric.clear()
             metric_name = str(metric)
             for outputs, targets in self.predict_generator(dataset, setname):
